64cc0d06940df82acbdf74a1d8de1846416d870a
soheil-ss <dev.soheil.ss@gmail.com>
2020-11-12 01:46:59 +0330

    night

---
+++ src/Components/OurProducts.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const OurProducts = () => {
+  return (
+    <div>
+      <section className="products">
+        <div className="section-title">
+          <h2> Our Products</h2>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default OurProducts;
