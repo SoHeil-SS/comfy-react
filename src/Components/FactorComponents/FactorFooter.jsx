173e39d418828363779e037d10bdd90846206e9c
soheil-ss <dev.soheil.ss@gmail.com>
2020-11-16 01:20:25 +0330

    details styled

--- src/Components/FactorComponents/FactorFooter.jsx
+++ src/Components/FactorComponents/FactorFooter.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useContext } from "react";
+
 import contexts from "../../contexts";
 
 const FactorFooter = () => {
--- src/Components/ProductsComponents/ProductItem.jsx
+++ src/Components/ProductsComponents/ProductItem.jsx
@@ -7,29 +7,42 @@ const ProductItem = ({
   image,
   alt,
   to,
-  style,
+  content,
+  productStyle,
   handleAddProduct,
   handlePath,
 }) => {
   return (
     <article className="product">
-      <div className="img-container">
+      <div className="img-container" style={productStyle && productStyle.div}>
         <Link to={to}>
           <img
-            style={style && style.img}
+            style={productStyle && productStyle.img}
             className="product-img"
             src={image}
             alt={alt}
             onClick={handlePath}
           />
         </Link>
-        <button onClick={handleAddProduct} className="bag-btn">
+        <button
+          style={productStyle && productStyle.button}
+          onClick={handleAddProduct}
+          className="bag-btn"
+        >
           {price}
           <i className="fas fa-shopping-cart">Add to cart</i>
           <i className="fas fa-shopping-cart"></i>
         </button>
       </div>
-      <h3 style={style && style.text}>{text}</h3>
+      <h3 style={productStyle && productStyle.text}>{text}</h3>
+      {content && (
+        <div style={{ padding: "10px" }}>
+          <h4>{text}</h4>
+          <div>
+            <h6>{content}</h6>
+          </div>
+        </div>
+      )}
     </article>
   );
 };
